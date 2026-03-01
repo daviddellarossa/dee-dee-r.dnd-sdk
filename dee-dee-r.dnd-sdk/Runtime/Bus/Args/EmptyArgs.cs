8b5c4e3fd1a41a3aa7fcd6f1d80cda990019bdee
Dee-Dee-R <david.dellarossa@proton.me>
2026-03-01 16:41:20 +0100

    Revert "Add message bus framework to DnD SDK"

--- dee-dee-r.dnd-sdk/Runtime/Bus/Args/EmptyArgs.cs
+++
@@ -1,7 +0,0 @@
-namespace DeeDeeR.DnD.Runtime.Bus.Args
-{
-    /// <summary>
-    /// Placeholder argument for parameterless signals and queries on the DnD SDK bus.
-    /// </summary>
-    public readonly struct EmptyArgs { }
-}
